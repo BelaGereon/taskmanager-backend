33a088fc65bc569b3e887b114e22927115e5f166
Bela Géréon <belagereon@gmx.de>
2025-05-21 12:57:19 +0200

    :recycle: refactor unit test name

--- src/test/java/com/example/taskmanager/model/TaskRequestDTOTest.java
+++ src/test/java/com/example/taskmanager/model/TaskRequestDTOTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TaskRequestDTOTest {
 
     @Test
-    void shouldCreateTaskRequestDTO() {
+    void shouldCreateTaskRequestDTOWithTheCorrectTitle() {
         TaskRequestDTO taskRequestDTO = new TaskRequestDTO();
 
         assertEquals("DTO Title", taskRequestDTO.getTitle());
