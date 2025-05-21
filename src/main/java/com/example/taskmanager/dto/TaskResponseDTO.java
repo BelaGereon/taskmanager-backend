1215dc7a08fbc23e2a008ca8f8cebb290654d9c7
Bela Géréon <belagereon@gmx.de>
2025-05-21 14:21:34 +0200

    :white_check_mark: create test for correctTaskResponseDTO construction

--- src/main/java/com/example/taskmanager/dto/TaskResponseDTO.java
+++ src/main/java/com/example/taskmanager/dto/TaskResponseDTO.java
@@ -1,7 +1,25 @@
 package com.example.taskmanager.dto;
 
 public class TaskResponseDTO {
+    private final int id;
+    private final String title;
+    private final String description;
+
+    public TaskResponseDTO(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
     public int getId() {
-        return 1;
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
--- src/test/java/com/example/taskmanager/dto/TaskResponseDTOTest.java
+++ src/test/java/com/example/taskmanager/dto/TaskResponseDTOTest.java
@@ -7,9 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TaskResponseDTOTest {
 
     @Test
-    void givenId_whenConstructingResponseDTO_thenIdIsCorrect() {
-        TaskResponseDTO taskResponseDTO = new TaskResponseDTO();
+    void givenParameters_whenConstructingResponseDTO_thenParametersAreCorrect() {
+        TaskResponseDTO taskResponseDTO1 =
+                new TaskResponseDTO(
+                        1,
+                        "Response DTO Title 1",
+                        "Response DTO Description 2"
+                );
+        TaskResponseDTO taskResponseDTO2 =
+                new TaskResponseDTO(
+                        2,
+                        "Response DTO Title 2",
+                        "Response DTO Description 2"
+                );
 
-        assertEquals(1, taskResponseDTO.getId());
+        assertEquals(1, taskResponseDTO1.getId());
+        assertEquals(1, taskResponseDTO1.getId());
+        assertEquals(1, taskResponseDTO1.getId());
+
+        assertEquals(2, taskResponseDTO2.getId());
+        assertEquals("Response DTO Title 2", taskResponseDTO2.getTitle());
+        assertEquals("Response DTO Description 2", taskResponseDTO2.getDescription());
     }
 }
