270a882bf046a662b94b373187fbfc588fb19d55
Bela Géréon <belagereon@gmx.de>
2025-05-22 15:30:16 +0200

    :white_check_mark: add toJson() method to response DTO

--- src/test/java/com/example/taskmanager/dto/TaskResponseDTOTest.java
+++ src/test/java/com/example/taskmanager/dto/TaskResponseDTOTest.java
@@ -1,5 +1,6 @@
 package com.example.taskmanager.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,5 +29,43 @@ public class TaskResponseDTOTest {
         assertEquals(2, taskResponseDTO2.id());
         assertEquals("Response DTO Title 2", taskResponseDTO2.title());
         assertEquals("Response DTO Description 2", taskResponseDTO2.description());
+    }
+
+    @Test
+    void givenTitleAndDescription_whenConvertingToJson_thenReturnValidJsonString() throws JsonProcessingException {
+        TaskResponseDTO requestDTO = new TaskResponseDTO(1, "Title", "Description");
+
+        String expectedJson = "{\"id\":1,\"title\":\"Title\",\"description\":\"Description\"}";
+
+        assertEquals(expectedJson, requestDTO.toJson());
+    }
+
+    @Test
+    void givenTitleAndDescription_whenConvertingToJson_thenReturnCorrectValuesInJsonString() throws JsonProcessingException {
+        int firstId = 1;
+        String firstTitle = "First Title";
+        String firstDescr = "First Description";
+
+        int secondId = 2;
+        String secondTitle = "Second Title";
+        String secondDescr = "Second Description";
+
+        TaskResponseDTO firstRequestDTO = new TaskResponseDTO(firstId, firstTitle, firstDescr);
+        TaskResponseDTO secondRequestDTO = new TaskResponseDTO(secondId, secondTitle, secondDescr);
+
+        String expectedJsonFirstRequest = "{" +
+                "\"id\":" + firstId + "," +
+                "\"title\":\"" + firstTitle + "\"," +
+                "\"description\":\"" + firstDescr + "\"" +
+                "}";
+
+        String expectedJsonSecondRequest = "{" +
+                "\"id\":" + secondId + "," +
+                "\"title\":\"" + secondTitle + "\"," +
+                "\"description\":\"" + secondDescr + "\"" +
+                "}";
+
+        assertEquals(expectedJsonFirstRequest, firstRequestDTO.toJson());
+        assertEquals(expectedJsonSecondRequest, secondRequestDTO.toJson());
     }
 }
